3ac3ac6a82ff215e84d71e8a6bd993e0a6d57979
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2023-07-27 22:09:58 +0800

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 判斷是否為開發中、產品路徑
+  base: process.env.NODE_ENV === "production" ? "/react-gh-pages-sample" : "/",
   plugins: [react()],
-})
+});
